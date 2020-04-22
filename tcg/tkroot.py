e232c8582ad958b8654b3659088401bcf5118e90
MeditativeSphynx <meditativesphynx@gmail.com>
2020-04-22 15:15:06 -0400

    add a shutdown callback for cleanup

--- tcg/tkroot.py
+++ tcg/tkroot.py
@@ -1,11 +1,21 @@
 import tkinter as tk
 from tkinter.ttk import Notebook
 
+from twisted.internet import reactor
+
 
 class Root(tk.Tk):
-    def __init__(self):
+    def __init__(self, reactor):
         super().__init__()
+        
+        self.reactor = reactor
+
         self.title('TCG')
         # self.geometry('500x500')
         self.notebook = Notebook(self)
         self.notebook.pack(fill=tk.BOTH, expand=1)
+
+    def shutdown_callback(self):
+        if tk.messagebox.askokcancel('Quit', 'Do you really wish to quit?'):
+            self.reactor.stop()
+            self.destroy()
