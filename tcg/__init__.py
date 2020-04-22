9738e21db884c06b4b319a84b85bb7fc6ec7d828
MeditativeSphynx <meditativesphynx@gmail.com>
2020-04-22 15:11:22 -0400

    fix multiple log entries

--- tcg/__init__.py
+++ tcg/__init__.py
@@ -1,19 +1,17 @@
 import os
+
 from loguru import logger
 
-# TODO: RENAME—REMOVE 'tcg'
 tcg_path = os.path.abspath(os.path.dirname(__file__))
-logger.debug(tcg_path)
 
-# TODO: RENAME—REMOVE 'tcg'
-# TODO: 
 tcg_pokemon_cache = f'{tcg_path}/data/pokecache.json'
 tcg_last_pokemon_accessed = f'{tcg_path}/data/last_pokemon.json'
 tcg_last_mtg_accessed = f'{tcg_path}/data/last_mtg.json'
 
-
-# TODO: FIX THE INITIAL CREATION OF THE `./logs` dir and the `debug.log` file
-if not os.path.exists(f'./logs/debug.log'):
+log_path = f'{tcg_path}/logs/debug.log'
+if not os.path.exists(log_path):
     # with open(f'./logs/debug.log', 'w') as f:
     #     f.write('')
-    logger.debug(f'{os.path.exists("./logs/debug.log")}')
+    logger.debug(f'{os.path.exists(log_path)}')
+
+logger.add(log_path, level='DEBUG')
